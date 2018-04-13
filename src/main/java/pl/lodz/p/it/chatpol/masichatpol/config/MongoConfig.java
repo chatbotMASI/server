3643c5f49d5e856ebae721179ddff97ff341f5cd
Dawid Dziedziczak <221587@edu.p.lodz.pl>
2018-04-13 15:19:05 +0200

    [MASBN-7] Dodano połączenie z bazą MongoDB oraz wstępną implementację kolekcji Log wraz z repozytorium do jej obsługi. #time 30m

---
+++ src/main/java/pl/lodz/p/it/chatpol/masichatpol/config/MongoConfig.java
@@ -0,0 +1,38 @@
+package pl.lodz.p.it.chatpol.masichatpol.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+@EnableMongoRepositories(basePackages="pl.lodz.p.it.chatpol.masichatpol.repositories")
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    private final Environment env;
+
+    @Autowired
+    public MongoConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public MongoClient mongoClient() { return new MongoClient("localhost", 27017); }
+    @Override
+    protected String getDatabaseName() { return "chatpolDB"; }
+}
