d11e33b0938bdf4f89eb653abd1f78090b48cf37
Dawid Dziedziczak <221587@edu.p.lodz.pl>
2018-05-06 15:37:30 +0200

    [MASBN-24] #time 5m Dodano konfigurację cors.

--- src/main/java/pl/lodz/p/it/chatpol/masichatpol/controllers/ChatController.java
+++ src/main/java/pl/lodz/p/it/chatpol/masichatpol/controllers/ChatController.java
@@ -14,6 +14,7 @@ public class ChatController {
     this.chatService = chatService;
   }
 
+  @CrossOrigin(origins = "http://localhost:3006")
   @RequestMapping(value = "/chat", method = RequestMethod.POST)
   public @ResponseBody
   MessageDto chat(@RequestBody(required = false) MessageDto message) {
